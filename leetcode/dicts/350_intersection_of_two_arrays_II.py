7bdb0ddd042fab4c7f615cd8630de78275c175d9
JFluo <luojianfeng2011@163.com>
2019-01-31 23:01:21 +0800

    practice

---
+++ leetcode/dicts/350_intersection_of_two_arrays_II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        from collections import Counter
+        return list((Counter(nums1) & Counter(nums2)).elements())
+
+
+def main():
+    args = [
+        ([], []),
+        ([], [1]),
+        ([1, 2, 2, 1], [2, 2]),
+        ([4, 9, 5], [9, 4, 9, 8, 4])
+    ]
+    s = Solution()
+    for num1, num2 in args:
+        print(s.intersection(num1, num2))
+
+
+if __name__ == '__main__':
+    main()
